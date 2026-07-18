2deb6dac2119e7e9a19c14427580eccb4fefa2d0
Diego Olivera <diegoolivera027@gmail.com>
2026-07-18 21:51:04 +1200

    test queries

---
+++ database/queries.sql
@@ -0,0 +1,8 @@
+INSERT INTO store (name, website) VALUES ('LiquorLand', 'https://www.liquorland.co.nz/');
+INSERT INTO store (name, website) VALUES ('SuperLiquor', 'https://www.liquorland.co.nz/');
+INSERT INTO store (name, website) VALUES ('The Bottle-O', 'https://www.liquorland.co.nz/');
+INSERT INTO store (name, website) VALUES ('Liquor Centre', 'https://www.liquorland.co.nz/');
+INSERT INTO store (name, website) VALUES ('Big Barrel', 'https://www.liquorland.co.nz/');
+INSERT INTO store (name, website) VALUES ('Black Bull Liquor', 'https://www.liquorland.co.nz/');
+
+SELECT * FROM store;
